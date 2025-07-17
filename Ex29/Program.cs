60a31d062e140fe586dec9ee17f0fa4ebb37dd7b
Juliana <julianatavaresdasilva@gmail.com>
2025-07-17 18:29:15 -0300

    project added

---
+++ Ex29/Program.cs
@@ -0,0 +1,27 @@
+﻿string path = @"D:\Udemy CSharp & OOP\meuarquivo.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<string> lista = new List<string>();
+        while (!sr.EndOfStream)
+        {
+            lista.Add(sr.ReadLine());
+        }
+
+        lista.Sort();
+
+        foreach (string linha in lista)
+        {
+            Console.WriteLine(linha);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred.");
+    Console.WriteLine(e.Message);
+}
+
+
